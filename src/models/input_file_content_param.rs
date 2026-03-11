1d060ff29806099d5cc1df0265426b4ca52eab2e
timvw <115225+timvw@users.noreply.github.com>
2026-03-11 04:17:45 +0000

    chore: update generated client from latest OpenAPI spec

--- src/models/input_file_content_param.rs
+++ src/models/input_file_content_param.rs
@@ -49,8 +49,6 @@ pub struct InputFileContentParam {
         skip_serializing_if = "Option::is_none"
     )]
     pub file_url: Option<Option<String>>,
-    #[serde(rename = "detail", skip_serializing_if = "Option::is_none")]
-    pub detail: Option<models::FileDetailEnum>,
 }
 
 impl InputFileContentParam {
@@ -62,7 +60,6 @@ impl InputFileContentParam {
             filename: None,
             file_data: None,
             file_url: None,
-            detail: None,
         }
     }
 }
