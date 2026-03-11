edcc0959dca486e6bba0ba6f45a861fa0ba3afcc
timvw <115225+timvw@users.noreply.github.com>
2026-03-11 04:17:45 +0000

    chore: update generated client from latest OpenAPI spec

--- src/models/input_file_content.rs
+++ src/models/input_file_content.rs
@@ -34,8 +34,6 @@ pub struct InputFileContent {
     /// The URL of the file to be sent to the model.
     #[serde(rename = "file_url", skip_serializing_if = "Option::is_none")]
     pub file_url: Option<String>,
-    #[serde(rename = "detail", skip_serializing_if = "Option::is_none")]
-    pub detail: Option<models::FileInputDetail>,
 }
 
 impl InputFileContent {
@@ -47,7 +45,6 @@ impl InputFileContent {
             filename: None,
             file_data: None,
             file_url: None,
-            detail: None,
         }
     }
 }
