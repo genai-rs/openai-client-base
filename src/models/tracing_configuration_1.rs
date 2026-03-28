8c8c227ee31cdae97bef278314d6ecf00bcd527a
timvw <115225+timvw@users.noreply.github.com>
2026-03-28 04:27:29 +0000

    chore: update generated client from latest OpenAPI spec

--- src/models/tracing_configuration_1.rs
+++ src/models/tracing_configuration_1.rs
@@ -11,8 +11,8 @@
 use crate::models;
 use serde::{Deserialize, Serialize};
 
-/// TracingConfiguration1 : Realtime API can write session traces to the [Traces Dashboard](/logs?api=traces). Set to null to disable tracing. Once tracing is enabled for a session, the configuration cannot be modified.  `auto` will create a trace for the session with default values for the workflow name, group id, and metadata.
-/// Realtime API can write session traces to the [Traces Dashboard](/logs?api=traces). Set to null to disable tracing. Once tracing is enabled for a session, the configuration cannot be modified.  `auto` will create a trace for the session with default values for the workflow name, group id, and metadata.
+/// TracingConfiguration1 : Realtime API can write session traces to the [Traces Dashboard](https://platform.openai.com/logs?api=traces). Set to null to disable tracing. Once tracing is enabled for a session, the configuration cannot be modified.  `auto` will create a trace for the session with default values for the workflow name, group id, and metadata.
+/// Realtime API can write session traces to the [Traces Dashboard](https://platform.openai.com/logs?api=traces). Set to null to disable tracing. Once tracing is enabled for a session, the configuration cannot be modified.  `auto` will create a trace for the session with default values for the workflow name, group id, and metadata.
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum TracingConfiguration1 {
