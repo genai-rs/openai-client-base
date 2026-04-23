9996662fa614eea9205561725d7824ee8417c9e9
timvw <115225+timvw@users.noreply.github.com>
2026-04-23 05:00:39 +0000

    chore: update generated client from latest OpenAPI spec

--- src/models/create_chat_completion_request.rs
+++ src/models/create_chat_completion_request.rs
@@ -195,7 +195,7 @@ impl CreateChatCompletionRequest {
 /// The retention policy for the prompt cache. Set to `24h` to enable extended prompt caching, which keeps cached prefixes active for longer, up to a maximum of 24 hours. [Learn more](/docs/guides/prompt-caching#prompt-cache-retention).
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub enum PromptCacheRetention {
-    #[serde(rename = "in-memory")]
+    #[serde(rename = "in_memory")]
     InMemory,
     #[serde(rename = "24h")]
     Variant24h,
